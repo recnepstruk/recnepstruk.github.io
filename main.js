63cf7f79e8df9a5522c864615a616a9b9276bd6a
kurt spencer <KurtSpencer@kurts-MacBook-Pro.local>
2017-01-11 21:32:24 -0700

    added sections

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// Scroll to top of window on refresh
+window.onload = function() {
+    setTimeout(function() {
+        scrollTo(0, 0);
+    }, 100); //100ms for example
+};
+
+// Scroll to first section
+$('#startScroll').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+    return false;
+});
+
+// Fade In components on scroll
 $(document).ready(function() {
 
     /* Every time the window is scrolled ... */
@@ -10,7 +24,7 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
+            if (bottom_of_window >= bottom_of_object) {
 
                 $(this).animate({ 'opacity': '1' }, 1000);
 
@@ -18,5 +32,27 @@ $(document).ready(function() {
 
         });
 
+    });
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function() {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
 });
